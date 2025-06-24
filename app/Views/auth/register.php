21da1e381d3d5fea081700b895614b51d701bc27
ByWildFrank <francomartinv14@gmail.com>
2025-06-24 01:46:51 -0300

    CSS minimo retocado - Dashboardddddddddddd

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Registro - BEAN</title>
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <style>
         <?php include 'register.css'; ?>
     </style>
